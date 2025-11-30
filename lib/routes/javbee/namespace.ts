2305a04c950ecc227e1fdc6a2046061701de79de
iovejieba <iovejieba@qq.com>
2025-11-30 17:22:25 +0800

    1

---
+++ lib/routes/javbee/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'javbee',
+    url: 'javbee.vip',
+    description: `::: tip
+官方未提供订阅源
+:::`,
+    lang: 'en',
+};
